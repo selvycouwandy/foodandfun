015c706a72b243e2d5528dd933f84910e01fc3b0
febriannewijaya <febywijaya@ymail.com>
2017-01-09 13:42:30 +0800

    home (visi dan misi)

---
+++ application/views/Home.php
@@ -0,0 +1,135 @@
+
+
+    <!-- Header -->
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+    
+        <div class="col-xs-12 col-sm-9"> 
+        <br><br><br><br>
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Kegiatan</button>
+          </p>
+          <div class="jumbotron">
+            <h1><span class=" glyphicon glyphicon-ice-lolly"></span>food & fun<span class=" glyphicon glyphicon-ice-lolly-tasted"></h1>
+           
+          </div>
+          <br>
+        <a name='about'></a>
+          <div class="row">
+            <div class="col-xs-6 col-lg-4">
+              <h2>Tentang UKM <span class=" glyphicon glyphicon-hand-left"></span></h2><br>
+              <p>UKM ini diberi nama "food & fun". Disini kita dapat bertukar pendapat dan berbagi pengalaman mengenai masakan yang pernah dibuat. Untuk saat ini anggota ukm berjumlah 42 orang.</p>
+              <br>
+
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Visi dan Misi <span class=" glyphicon glyphicon-flag"></span></h2> <br> 
+              <p>Visi UKM : dengan adanya ukm ini mahasiswa bisa menjadi lebih berpengalaman dalam mengolah masakan.</p>
+              <p>Misi UKM : menyelenggarakan kegiatan masak memasak antar mahasiwa dan saling mencicipi resep yang telah dimasak satu sama lain.</p>
+              <br>
+
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+              <h2>Organisasi dan Pengurus <span class=" glyphicon glyphicon-user"></span></h2><br>
+              <p>Ketua UKM: William Tumewah </p>
+              <p>Sekretaris UKM: Kevin Changir </p>
+              <p>Pembimbing UKM: Ibu Husni Anggriani </p>
+              <br><br><br><br>
+              <br>
+              <a name='form'></a>
+            
+
+            
+
+
+              
+              </div><!--/.col-xs-6.col-lg-4-->
+          </div><!--/row-->
+        </div><!--/.col-xs-12.col-sm-9-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+         
+
+
+          <div id="kegiatan">
+          <br><br>
+            <h2>Kegiatan1 <span class=" glyphicon glyphicon-chevron-down"></span></h2>
+              <p>Dilakukan rapat pertama kali tanggal 16 November 2016 pukul 12.09 di ruang 203. rapat membahas tentang pemilihan ketua ukm dan sekretaris,serta menentukan nama ukm. </p>
+              <h2>Kegiatan2 <span class=" glyphicon glyphicon-chevron-down"></span></h2>
+              <p>Mengumpulkan resep makanan lalu dipilih untuk dijadikan praktek pertama di ukm. praktek dilakukan dirumah masing-masing lalu dibawa hasilnya pada pertemuan selanjutnya. </p>
+              <h2>Kegiatan3 <span class=" glyphicon glyphicon-chevron-down"></span></h2>
+              <p>Mengumpulkan hasil dari praktek yang dilakukan dirumah</p>
+  
+          </div>
+        </div><!--/.sidebar-offcanvas-->
+      </div><!--/row-->
+<br><br>
+      <hr>
+        <a></a>
+            <div class= "col-lg-8">
+            <h1> Contact Us</h1>
+            <p> <a href="http://www.kharisma.ac.id/" target="_blank">STMIK KHARISMA</a></p>
+      <p><a href="https://www.facebook.com/stmikkharismamks" target="_blank">Facebook</a> Fan Page STMIK KHARISMA</p>
+      
+            </div>
+      <footer>
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+
+    </div><!--/.container-->
+
+
+     <div class="portfolio-modal modal fade" id="login" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <h4 class="modal-title" id="exampleModalLabel">Sign In</h4>
+                                </div>
+
+                                  <div class="modal-body">
+                                    <form>
+                                      <div class="form-group">
+                                        <div class="form-group">
+
+                             
+                                      <label for="email" class="cols-sm-2 control-label">Your Email</label>
+                                      <div class="cols-sm-10">
+                                        <div class="input-group">
+                                          <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                                          <input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+                                        </div>
+                                      </div>
+                                    </div>
+
+          
+                                    <label for="password" class="cols-sm-2 control-label">Password</label>
+                                    <div class="cols-sm-10">
+                                      <div class="input-group">
+                                        <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                        <input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div class="modal-footer">
+                                    <button class="btn btn-primary" type="submit">Sign In</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
