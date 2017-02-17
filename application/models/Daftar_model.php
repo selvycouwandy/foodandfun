3e1f2c97656d24cea3433c5633859ae14180f095
selvycouwandy <selvycouwandy@ymail.com>
2017-02-16 19:41:03 -0800

    Update Daftar_model.php

--- application/models/Daftar_model.php
+++ application/models/Daftar_model.php
@@ -3,25 +3,5 @@
 class Daftar_model extends CI_Model{
 
 
-	public function __contruct(){
-		parent::_construct();
-
-	}
-
-	public function insert_user()
-	{
-		$data =
-		[	'namalengkap'		=> $this->input->post('namalengkap'),
-			'nim'				=> $this->input->post('nim'),
-			'username'			=> $this->input->post('username'),
-			'alamat'			=> $this->input->post('alamat'),
-			'telepon'			=> $this->input->post('telepon'),
-			'email'				=> $this->input->post('email'),
-			];
-
-		$this->db->insert('pendaftaran', $data);
-
-
-
-	}
+	
 }
