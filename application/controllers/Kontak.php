6c684665c4adbd1ebf48b2f445825e598ea43078
selvycouwandy <selvycouwandy@ymail.com>
2017-02-16 19:29:14 -0800

    Update Kontak.php

--- application/controllers/Kontak.php
+++ application/controllers/Kontak.php
@@ -6,9 +6,7 @@ class Kontak extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('templates/header');
-		$this->load->view('templates/nav');	
-		$this->load->view('kontak');
-		$this->load->view('templates/footer');
+		
+		
 	}
 }
