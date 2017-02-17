af0d16ffaca93b1d3d09bf51a4fbc20a948228df
selvycouwandy <selvycouwandy@ymail.com>
2017-02-16 19:58:06 -0800

    upload daftar model

--- application/models/Daftar_model.php
+++ application/models/Daftar_model.php
@@ -3,5 +3,35 @@
 class Daftar_model extends CI_Model{
 
 
-	
+	public function __contruct(){
+		parent::_construct();
+
+	}
+
+	public function insert_user()
+	{
+		$data =
+		[	'namalengkap'		=> $this->input->post('namalengkap'),
+			'nim'				=> $this->input->post('nim'),
+			'alamat'			=> $this->input->post('alamat'),
+			'telepon'			=> $this->input->post('telepon'),
+			'email'				=> $this->input->post('email'),
+			'tanggallahir'		=> $this->input->post('tanggallahir'),
+			];
+
+		$this->db->insert('pendaftaran', $data);
+	}
+
+
+    public function nim_sudah_terdaftar( $nim ){
+        $query = $this->db->get_where( 'pendaftaran', array( 'nim' => $nim) );
+        if( !empty( $query->row_array() ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    function tampil_info(){
+		return $this->db->get('pendaftaran');
+	}
 }
