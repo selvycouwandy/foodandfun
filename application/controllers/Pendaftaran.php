21bc2117c2f408fbb81af8bebba505d613ba6efa
selvycouwandy <selvycouwandy@ymail.com>
2017-02-16 19:45:23 -0800

    upload pendaftaran.php

--- application/controllers/Pendaftaran.php
+++ application/controllers/Pendaftaran.php
@@ -4,7 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pendaftaran extends CI_Controller {
 
-	
-	
+	public function __construct(){
+		parent:: __construct();
+		$this->load->model('daftar_model');
+	}
+
+	public function index()
+	{
+		$this->load->view('templates/header');
+		$this->load->view('templates/nav');	
+		$this->load->view('pendaftaran');
+		$this->load->view('templates/footer');
+	}
+
+	public function prosespendaftaran(){
+//		var_dump($this->input->post('namalengkap'));
+		$this->daftar_model->insert_user();
+		redirect(base_url());
+	}
 	
 }
