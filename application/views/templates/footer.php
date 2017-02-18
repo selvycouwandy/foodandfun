22cebfe47bb9ef48de94b1b1f4e9014987da8b5c
selvycouwandy <selvycouwandy@ymail.com>
2017-02-17 20:02:32 -0800

    update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,19 @@
     
     <!-- jQuery -->
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
     <!-- Contact Form JavaScript -->
-    <script src="assets/js/jqBootstrapValidation.js"></script>
-    <script src="assets/js/contact_me.js"></script>
+    <script src="<?=base_url()?>assets/js/jqBootstrapValidation.js"></script>
+    <script src="<?=base_url()?>assets/js/contact_me.js"></script>
 
     <!-- Theme JavaScript -->
-    <script src="assets/js/agency.min.js"></script>
+    <script src="<?=base_url()?>assets/js/agency.min.js"></script>
 
 </body>
 
