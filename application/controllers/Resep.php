3c55084a5f549ec52f0642d0a2c6fcc1dd943da3
febriannewijaya <febywijaya@ymail.com>
2017-02-21 21:08:01 -0800

    Add files via upload

--- application/controllers/Resep.php
+++ application/controllers/Resep.php
@@ -3,10 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Resep extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();		
+		$this->load->model('m_resep');
+        $this->load->helper('url');
+	}
+
 
 	public function index()
 	{
-		
-		
+		$data['resep'] = $this->m_resep->tampil_info()->result();
+		$this->load->view('templates/header');
+		$this->load->view('templates/nav');	
+		$this->load->view('resep',$data);
+		$this->load->view('templates/footer');
 	}
 }
