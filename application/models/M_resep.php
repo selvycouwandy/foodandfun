ab393489d260ee95d66059a87ebe046ec5916c3f
febriannewijaya <febywijaya@ymail.com>
2017-02-21 21:13:30 -0800

    upload model resep

---
+++ application/models/M_resep.php
@@ -0,0 +1,35 @@
+<?php
+
+class M_resep extends CI_Model{
+
+	function tampil_info(){
+		$this->db->select('*');
+		//$this->db->from('resep');
+		$this->db->order_by("id","desc");
+		return $this->db->get('resep');
+	}
+
+	function ambil_data(){
+		return $this->db->get('resep');
+	}
+
+	function input_data($data,$table){
+		$this->db->insert($table,$data);
+	}
+	
+
+	function hapus_data($where,$table){
+		$this->db->where($where);
+		$this->db->delete($table);
+	}
+
+	function edit_data($where,$table){		
+		return $this->db->get_where($table,$where);
+	}
+	
+	function update_data($where,$data,$table){
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}
+
+}
