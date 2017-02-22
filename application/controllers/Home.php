b5db0e85b429c9e690a26b8069439211e2bc6269
selvycouwandy <selvycouwandy@ymail.com>
2017-02-21 22:06:47 -0800

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,12 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();		
+		$this->load->model('m_home');
+        	$this->load->helper('url');
+	}
+
 
 	public function index()
 	{
+		$data['kegiatan'] = $this->m_home->tampil_info()->result();
+		$data['struktur'] = $this->m_home->tampil_s()->result();
 		$this->load->view('templates/header');
 		$this->load->view('templates/nav');	
-		$this->load->view('Home');
+		$this->load->view('Home',$data);
 		$this->load->view('templates/footer');
 	}
 }
+
