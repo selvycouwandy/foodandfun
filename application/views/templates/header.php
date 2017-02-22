e3ddf7cd832414fe04d31f6641c51782a1a09390
selvycouwandy <selvycouwandy@ymail.com>
2017-02-21 23:21:24 -0800

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,10 +12,15 @@
     <title>food and fun</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?=base_url()?>assets/css/bootstrap.css" rel="stylesheet">
+	
+    <link href="
+<?php  echo base_url('assets/css/bootstrap.css'); ?>
+" rel="stylesheet">
+
+    
 
     <!-- Custom Fonts -->
-    <link href="<?=base_url()?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
@@ -23,9 +28,9 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
     <!-- Theme CSS -->
-    <link href="<?=base_url()?>assets/css/agency.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/agency.css'); ?>" rel="stylesheet">
 
-    <link href="<?=base_url()?>assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -33,7 +38,14 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+	
+	<!-- by anne for like -->
+		<link href="<?php echo base_url('assets/asset_forlike/css/styleforlike.css'); ?>" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="<?php echo base_url('assets/asset_forlike/js/jquery.min.js'); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('assets/asset_forlike/js/function3.js'); ?>"></script>
+	<!-- by anne for like -->
+	
+	
 </head>
 
 <body id="page-top" class="index">
