bffb9afb5d8e1504a2f352ba172df2161ecd90b0
selvycouwandy <selvycouwandy@ymail.com>
2017-02-21 23:20:11 -0800

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,21 @@
     
     <!-- jQuery -->
-    <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
     <!-- Contact Form JavaScript -->
-    <script src="<?=base_url()?>assets/js/jqBootstrapValidation.js"></script>
-    <script src="<?=base_url()?>assets/js/contact_me.js"></script>
+    <script src="<?php echo base_url('assets/js/jqBootstrapValidation.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/contact_me.js'); ?>"></script>
 
     <!-- Theme JavaScript -->
-    <script src="<?=base_url()?>assets/js/agency.min.js"></script>
+    <script src="<?php echo base_url('assets/js/agency.min.js'); ?>"></script>
+    
+    <script src="<?php echo base_url('assets/js/ajax.js'); ?>"></script>
 
 </body>
 
