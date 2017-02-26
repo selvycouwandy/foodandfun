557e53ed4e7c6a03437d12db6aaf7134f00f65ce
febriannewijaya <febywijaya@ymail.com>
2017-02-26 23:58:19 +0800

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,9 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-
+//kiri= user liat , kanan = file controller/metode
+//kl tdk ada metode, artix itu function index
 $route['default_controller'] = 'home';
-$route['resep'] = 'resep';
+//$route['resep'] = 'resep';
+$route['resep1'] = 'resep1';
+
 $route['pendaftaran'] = 'pendaftaran';
 $route['prosespendaftaran'] = 'pendaftaran/prosespendaftaran';
 $route['kontak'] = 'kontak';
@@ -18,13 +21,20 @@ $route['admin'] = 'admin';
 $route['admin/login'] = 'admin/aksi_login';
 $route['admin/logout'] = 'admin/logout';
 $route['admin_home'] = 'admin_home';
-$route['admin_resep'] = 'admin_resep';
-$route['admin_resep/tambah'] = 'admin_resep/tambah';
+//$route['admin_resep'] = 'admin_resep';
+$route['admin_resep1'] = 'admin_resep1';
+
+
+//$route['admin_resep/tambah'] = 'admin_resep/tambah';
+$route['admin_resep1/tambah'] = 'admin_resep1/tambah';
+
+
 $route['admin_anggota'] = 'admin_anggota';
 
 
 
 /***********AJAX******************/
+
 $route['ajax/ceknim'] = 'ajax/ceknimnim';
 
 /********************************/
